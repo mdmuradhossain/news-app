f6f699b80ad46fa720eca6f752938149b0a17753
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-22 13:26:57 +0600

    project created

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '7443707e1fe847d59782363093292211',
 };
 
 /*
